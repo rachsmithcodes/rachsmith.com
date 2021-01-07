268baea81a33e02dc5335a47ef356039dff55832
rachsmithcodes <contact@rachsmith.com>
2021-01-07 20:48:42 +1000

    fix page template

--- src/templates/page.js
+++ src/templates/page.js
@@ -14,6 +14,7 @@ import { MetaData } from '../components/common/meta';
  */
 const Page = ({ data, location }) => {
   const page = data.ghostPage;
+  const tags = data.allGhostTag.edges.map((edge) => edge.node);
 
   return (
     <>
@@ -21,7 +22,7 @@ const Page = ({ data, location }) => {
       <Helmet>
         <style type="text/css">{`${page.codeinjection_styles}`}</style>
       </Helmet>
-      <Layout>
+      <Layout tags={tags}>
         <div className="container">
           <article className="content">
             <h1 className="content-title">{page.title}</h1>
@@ -46,6 +47,7 @@ Page.propTypes = {
       html: PropTypes.string.isRequired,
       feature_image: PropTypes.string,
     }).isRequired,
+    allGhostTag: PropTypes.object.isRequired,
   }).isRequired,
   location: PropTypes.object.isRequired,
 };
