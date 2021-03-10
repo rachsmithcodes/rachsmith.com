a0fe82f50e369645403f5a2fafdb45a49b3e9b7c
rachsmithcodes <contact@rachsmith.com>
2021-03-10 11:37:37 +1000

    prettier

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -1,36 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
 
 const Pagination = ({ pageContext }) => {
-    const { previousPagePath, nextPagePath, humanPageNumber, numberOfPages } = pageContext
+  const {
+    previousPagePath,
+    nextPagePath,
+    humanPageNumber,
+    numberOfPages,
+  } = pageContext;
 
-    return (
-        <nav className="pagination" role="navigation">
-            <div>
-                {previousPagePath && (
-
-                    <Link to={previousPagePath} rel="prev">
-                            Previous
-                    </Link>
-
-                )}
-            </div>
-            {numberOfPages > 1 && <div className="pagination-location">Page {humanPageNumber} of {numberOfPages}</div>}
-            <div>
-                {nextPagePath && (
-
-                    <Link to={nextPagePath} rel="next">
-                            Next
-                    </Link>
-                )}
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="pagination" role="navigation">
+      <div>
+        {previousPagePath && (
+          <Link
+            to={previousPagePath}
+            rel="prev"
+            className="pagination-link later-posts"
+          >
+            &laquo; Later Posts
+          </Link>
+        )}
+      </div>
+      {numberOfPages > 1 && (
+        <div className="pagination-location">
+          Page {humanPageNumber} of {numberOfPages}
+        </div>
+      )}
+      <div>
+        {nextPagePath && (
+          <Link
+            to={nextPagePath}
+            rel="next"
+            className="pagination-link earlier-posts"
+          >
+            Earlier Posts &raquo;
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 Pagination.propTypes = {
-    pageContext: PropTypes.object.isRequired,
-}
+  pageContext: PropTypes.object.isRequired,
+};
 
-export default Pagination
+export default Pagination;
