a66fddcdaeae7529a292f6dfe76bb57753822264
Rachel Smith <contact@rachsmith.com>
2022-11-19 07:46:15 +1000

    add note and RSS comment prompt

--- src/pages/rss.js
+++ src/pages/rss.js
@@ -6,8 +6,19 @@ const posts = Object.values(postImportResult);
 
 const postsWithContent = await Promise.all(
   posts.map(async (post) => {
-    const rawContent = await post.rawContent();
+    let rawContent = await post.rawContent();
+
+    const titleEncoded = encodeURIComponent(`re: ${post.frontmatter.title}`);
+    const tweetTextEncoded = encodeURIComponent(
+      `re: https://rachsmith.com/${post.frontmatter.slug}`
+    );
+
     let html = marked.parse(rawContent);
+
+    html += `
+      <hr />
+      <p>Thanks for reading this post via RSS! Let me know your thoughts by leaving a comment on the <a href="https://rachsmith.com/${post.frontmatter.slug}">original post</a>, sending <a href="mailto:contact@rachsmith.com?subject=${titleEncoded}>me an email</a>, or <a href="https://twitter.com/intent/tweet?screen_name=rachsmithtweets&text=${tweetTextEncoded}">Tweet at me<a/>.</p>
+    `;
 
     return {
       ...post,
@@ -20,13 +31,15 @@ postsWithContent.sort((a, b) => {
   return new Date(b.frontmatter.added) - new Date(a.frontmatter.added);
 });
 
+const postsToRender = postsWithContent.slice(0, 20);
+
 export const get = () =>
   rss({
     title: "Rach Smith's digital garden",
     description:
       "Hi 👋🏼 I'm Rach. A developer building software for CodePen, wife, mother of two, productivity nerd and recovering screen addict. This is my digital garden.",
     site: import.meta.env.SITE,
-    items: postsWithContent.map((post, i) => {
+    items: postsToRender.map((post, i) => {
       const categoryTags = post.frontmatter.tags
         .map((tag) => `<category><![CDATA[${tag}]]></category>`)
         .join('');
